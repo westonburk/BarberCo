8c8b76486adf1098ea13617a860de2854485492e
westonburk <108580896+westonburk@users.noreply.github.com>
2024-05-11 17:19:25 -0400

    working on jwts

---
+++ BarberCo.Api/Auth/JwtHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BarberCo.Api.Auth
+{
+    public class JwtHelper
+    {
+        public string GenerateJWTToken(SystemUser user, IConfiguration configuration)
+        {
+            var jwtKey = configuration.GetSection("Jwt:Key").Get<string>();
+            var claims = new List<Claim> 
+            {
+                new (ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new (ClaimTypes.Name, user.Name),
+            };
+
+            var jwtToken = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(30),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(
+                       Encoding.UTF8.GetBytes(jwtKey)
+                        ),
+                    SecurityAlgorithms.HmacSha512Signature)
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+
+            return token;
+        }
+    }
+
+    // TODO: change this to use 
+    public class SystemUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- BarberCo.Api/Program.cs
+++ BarberCo.Api/Program.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+builder.Services.AddAuthentication(cfg =>
+{
+    cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ClockSkew = TimeSpan.Zero,
+        };
+    });
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +45,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
