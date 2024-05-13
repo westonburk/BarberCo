a1b0941ddf1281340a2216e53f005ec357b04088
westonburk <108580896+westonburk@users.noreply.github.com>
2024-05-13 18:34:36 -0400

    working on barber endpoint

--- BarberCo.Api/Controllers/BarberController.cs
+++ BarberCo.Api/Controllers/BarberController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BarberCo.Api.Dtos;
+using BarberCo.SharedLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarberCo.Api.Controllers
@@ -7,6 +11,46 @@ namespace BarberCo.Api.Controllers
     [ApiController]
     public class BarberController : ControllerBase
     {
+        private readonly UserManager<Barber> _userManager;
 
+        public BarberController(UserManager<Barber> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("register")]
+        public async Task<ActionResult<Barber>> RegisterBarber([FromBody] BarberRegistrationDto barberDto)
+        {
+            if (barberDto.Password != barberDto.PasswordConfirm)
+            {
+                return BadRequest($"{nameof(barberDto.Password)} and {barberDto.PasswordConfirm} did not match.");
+            }
+
+            // TODO: refactor this into a repo
+            var newBarber = new Barber { UserName = barberDto.UserName, Email = barberDto.Email, PhoneNumber = barberDto.PhoneNumber };
+            var result = await _userManager.CreateAsync(newBarber, barberDto.Password);
+            if (result.Succeeded == false)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            var roleResult = await _userManager.AddToRoleAsync(newBarber, barberDto.Role);
+            if (roleResult.Succeeded == false)
+            {
+                return BadRequest(roleResult.Errors);
+            }
+
+            return Ok(newBarber);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Barber>>> GetBarbers()
+        {
+            // TODO: fetch all barbers from repo
+            await Task.CompletedTask;
+
+            return Ok(new List<Barber>());
+        }
     }
 }
