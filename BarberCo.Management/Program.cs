2081e49ec27c43a53101471f6fa950bc00d767ba
westonburk <108580896+westonburk@users.noreply.github.com>
2025-09-07 16:32:55 -0400

    adding local storage

--- BarberCo.Management/Program.cs
+++ BarberCo.Management/Program.cs
@@ -1,4 +1,5 @@
 using BarberCo.Management;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+
+
 
 await builder.Build().RunAsync();
