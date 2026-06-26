2f8b24401879ccadab339564d11e6db041197789
westonburk <108580896+westonburk@users.noreply.github.com>
2026-06-26 12:52:51 -0400

    added db migrations

---
+++ BarberCo.DataAccess/Migrations/20260626161317_ChangesForSMSConfirmationFeature.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberCo.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesForSMSConfirmationFeature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "confirmation_code_hash",
+                table: "appointments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "confirmation_failed",
+                table: "appointments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "confirmed_on",
+                table: "appointments",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "created_by",
+                table: "appointments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_on",
+                table: "appointments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "confirmation_code_hash",
+                table: "appointments");
+
+            migrationBuilder.DropColumn(
+                name: "confirmation_failed",
+                table: "appointments");
+
+            migrationBuilder.DropColumn(
+                name: "confirmed_on",
+                table: "appointments");
+
+            migrationBuilder.DropColumn(
+                name: "created_by",
+                table: "appointments");
+
+            migrationBuilder.DropColumn(
+                name: "created_on",
+                table: "appointments");
+        }
+    }
+}
