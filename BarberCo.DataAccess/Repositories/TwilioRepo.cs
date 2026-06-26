0a88f7944a0c8e33c5b73032afdabf80c377e987
westonburk <108580896+westonburk@users.noreply.github.com>
2026-06-26 12:12:06 -0400

    remove using

--- BarberCo.DataAccess/Repositories/TwilioRepo.cs
+++ BarberCo.DataAccess/Repositories/TwilioRepo.cs
@@ -7,7 +7,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.WebRequestMethods;
 
 namespace BarberCo.DataAccess.Repositories
 {
